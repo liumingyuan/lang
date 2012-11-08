8ece8e9c8e232848013c3c1afd6cf7f44b84a318
liumingyuan <lzc.ustc@gmail.com>
2012-11-08 20:30:10 +0800

    learn the object oriented python

---
+++ python/lpthw/ex44.py
@@ -0,0 +1,52 @@
+﻿# -*- encoding:utf-8 -*-
+
+class Parent(object):
+    
+    def __init__(self):
+        print "Parent init()"
+    
+    def implicit(self):
+        print "Parent Implicit()"
+    
+    def override(self):
+        print "Parent override()"
+    
+    def altered(self):
+        print "Parent Altered"
+    
+    
+
+class Child(Parent):
+    
+    def override(self):
+        print "Child override()"
+    
+    def altered(self):
+        print "Child before altered()"
+        super(Child, self).altered()
+        print "Child after altered()"
+    
+    def __init__(self, stuff):
+        self.stuff = stuff
+        super(Child, self).__init__()
+    
+
+
+dad = Parent()
+son = Child("cool")
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
+
+
+
+
+
+
+
